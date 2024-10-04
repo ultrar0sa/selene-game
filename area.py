f69909209661552326298b1aadaa4dd7d1678050
ultrar0sa <jade.lukken@gmail.com>
2024-10-03 22:23:29 -0500

    added more comments everywhere

--- area.py
+++ area.py
@@ -3,6 +3,7 @@ class Area:
     # description = ""
     # gates = []  #list of avalible areas accessible from this area.
     # flags = {}  dictionaries with the key being the flag name and the value being a boolean or other value.
+    # avaliable_targets = {} dict, key the name of the target allowed, and the value being the target function needed to be run.
     def __init__(self, name, description, gates, flags = {}, avaliable_targets = {}, mapPath=""):
         self.mapPath = mapPath
         self.names = name
--- main.py
+++ main.py
@@ -6,13 +6,6 @@ from game import Game
 from area import Area
 VERSION = "Early Alpha"
 
-#VALID_PLAYER_ACTIONS = {"push" : push(), "pull" : pull(), "lie" : lie(), "cheat" : cheat(), "kill" : kill(), "look" : look()}
-#VALID_NO_TARGET_COMMANDS = ["look"]
-#VALID_TARGET_OBJECTS = ["button", "keypad"]
-#VALID_ACCESSORY_OBJECTS = ["hammer", "fist"]
-#playerAction = "" #may need to refactor this into a class later
-#playerTargetObject = ""
-#playerAccessoryObject = ""
 
 #basic start menu
 def start_menu():
@@ -23,7 +16,7 @@ def start_menu():
             start_menu()
         case "2":
             print("When prompted, type commands into the console and press ENTER. \nThese commands include:\nmove {location name}\npush {button name}\nand fix {object}")
-            print("The \"map\" command draws a map in the turtle window.")
+            print("The \"map\" command draws a map in the turtle window. NOTE: Map window cannot be closed by player using window close button. \nTo exit the game, player must hit ctrl-c and then enter.")
             print("Try and experiment with potential commands to see if they work in potential situtations!")
             print("Paper and pencil is recommended to have on hand.")
             start_menu()
@@ -42,13 +35,14 @@ from routines.targets import button
 oxygenTubeFixed = False
 launchReady = False
 BUTTON_DICT = {"jdry" : button, "uo13" : button, "vk88" : button, "jn34" : button, "wz81" : button, "vo29" : button, "fk73" : button, "kj22" : button, "mo11" : button, "we64" : button, "lq12" : button, "ii43" : button, "js01" : button, "cf04" : button, "sz24" : button, "ez59" : button, "n57x" : button, "msy4" : button, "aym1" : button, "0frz" : button, "dyjj" : button, "jv71" : button, "4lhm" : button, "fdcx" : button, "ut9c" : button, "8rsl" : button, "ra78" : button, "jmn8" : button, "rwb5" : button, "xuu6" : button, "sgub" : button, "awpz" : button, "rrdz" : button, "mdna" : button, "wa03" : button, "ut13" : button, "zo51" : button, "ef57" : button, "uj34" : button, "su33" : button}
-cape = Area(["cape", "mission control"], "this is cape canveral, the starting point in your journey", [], mapPath="savedshapes/capemap.json")
 
-tower = Area(["tower"], "this is tower", [], mapPath="savedshapes/towermap.json")
+#area creation
+cape = Area(["cape", "mission control"], "this is cape canveral, the starting point in your journey", [], mapPath="savedshapes/capemap.json")
+tower = Area(["tower, pad"], "this is tower", [], mapPath="savedshapes/towermap.json")
 capsule = Area(["capsule", "inside"], "this is the space capsule", [], {"launchReady" : launchReady}, mapPath="savedshapes/capsulemap.json")
 dockingPort = Area(["docking port", "dock"], "you are next to the [docking port], out in space. the broken oxygen tube is right in front of you.", [], {"fixed" : oxygenTubeFixed}, mapPath="savedshapes/capsulemap.json")
 outsideCapsuleDoor = Area(["outside capsule door", "outside", "spacewalk", "space", "capsule door"], "you are out in space next to the [capsule door]. the broken oxygen tube is right next to the [docking port]", [], mapPath="savedshapes/capsulemap.json")
-capsule.avaliable_targets = BUTTON_DICT
+capsule.avaliable_targets = BUTTON_DICT 
 dockingPort.avaliable_targets = {}
 capsule.gates = outsideCapsuleDoor.names + cape.names + tower.names
 dockingPort.gates = outsideCapsuleDoor.names
@@ -63,17 +57,17 @@ AREA_DICT = {"capsule" : capsule, "inside" : capsule,
              "docking port" : dockingPort, "dock" : dockingPort,
              "cape" : cape, "mission control" : cape,
              "tower" : tower}
-MAP_DICT = {"capsule" : "savedshapes/capsulemap.json", "cape" : "savedshapes/capemap.json", "tower" : "savedshapes/towermap.json"}
+
 game = Game(capsule)
 Game.AREA_DICT = AREA_DICT
-Game.MAP_DICT = MAP_DICT
 
-print(capsule.names[0])
+
+
 print("SELENE " + str(VERSION))
 
 start_menu()
 
-
+#basic starting senario
 print("you are just about to your moon burn calculations that will put you in the moons orbit!")
 game.add_radio_notification("you see the \"incomming message\" light fire on your dashboard", "HOUSTON: we've detected a leak in one of the oxygen tubes out by the dock. please go out and fix it before you get ready for launch!")
 while not Game.gameOver:
